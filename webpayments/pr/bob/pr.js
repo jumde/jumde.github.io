257aeb5c6ed28794a83f8fb22eb2356d6b2eefe6
Pranjal Jumde <pranjal.jumde@gmail.com>
2019-10-15 17:24:33 -0700

    new manifest

--- webpayments/pr/bob/pr.js
+++ webpayments/pr/bob/pr.js
@@ -8,7 +8,7 @@ function buildPaymentRequest() {
   }
 
   const supportedInstruments = [{
-    supportedMethods: 'https://bobpay.xyz/pay'
+    supportedMethods: 'https://jumde.github.io/webpayments/pay'
   },
   {
     supportedMethods: 'interledger'
